a355bec40eee4911f0a89c9fca06223f09d5c244
VET4 <wlgnsxcv@naver.com>
2022-09-13 19:54:19 +0900

    add Description

--- 7segment_1/dut_7segment_1.v
+++ 7segment_1/dut_7segment_1.v
@@ -16,6 +16,7 @@ module dut_7segment(
 	integer count = 0;
 	reg [7:0] s;
 
+// 리셋 여부에 대한 내부 count 동작 선언
 always @ (posedge clk) begin
 	if (rst == 1) begin
 		count = 0;
@@ -28,19 +29,21 @@ always @ (posedge clk) begin
 	end
 end
 
+// count 값에 따른 7-segment 선언
 always @ (negedge clk) begin
 	s <= 	(count == 0) ? 8'b11111100: //0
-		(count == 1) ? 8'b01100000: //1
-		(count == 2) ? 8'b11011010: //2
-		(count == 3) ? 8'b11110010: //3
-		(count == 4) ? 8'b01100110: //4
-		(count == 5) ? 8'b10110110: //5
-		(count == 6) ? 8'b10111110: //6
-		(count == 7) ? 8'b11100000: //7
-		(count == 8) ? 8'b11111110: //8
-		(count == 9) ? 8'b11100110: 8'b00000000; // 9, error: 0
+			(count == 1) ? 8'b01100000: //1
+			(count == 2) ? 8'b11011010: //2
+			(count == 3) ? 8'b11110010: //3
+			(count == 4) ? 8'b01100110: //4
+			(count == 5) ? 8'b10110110: //5
+			(count == 6) ? 8'b10111110: //6
+			(count == 7) ? 8'b11100000: //7
+			(count == 8) ? 8'b11111110: //8
+			(count == 9) ? 8'b11100110: 8'b00000000; // 9, error: 0
 end
 
+// 최종 output인 seg에 값 할당
 assign seg = s; 
 
 endmodule
