081d7d9ee5ad4cb6020dc177c2add7a5709ef615
Jihun Noh <korjihu@gmail.com>
2022-09-18 21:10:21 +0900

    Reduce Codes

--- 7segment_3/dut_7segment_3.v
+++ 7segment_3/dut_7segment_3.v
@@ -2,7 +2,8 @@
 // TSN Lab 미니인턴
 // 추가 과제 2
 // 작성: 노지훈
-// 1차 완성일: 
+// 1차 완성일: 20220917
+// 2차 완성일: 20220918
 ///////////////////////////////////////
 
 // 현실시간과 매칭을 위해 1초 단위로 설정
@@ -16,7 +17,7 @@ module dut_7segment_3(
 	output [7:0] seg1,
 	output [7:0] seg2
 );
-	// 000 ~ 999까지 세는 카운터용 변수 선언
+	// 000 ~ 599까지 세는 카운터용 변수 선언
 	integer total_count = 0;
 	// 자릿수 별 값 할당을 위한 변수 선언
 	integer count0 = 0;
@@ -30,14 +31,34 @@ module dut_7segment_3(
 	reg [7:0] s1;
 	reg [7:0] s2;
 
+// 반복작업 task로 정의
+task digit_converter(
+	input [31:0] count,
+	output [7:0] digit
+);
+	case (count)
+		0	: digit = "0";
+		1	: digit = "1";
+		2	: digit = "2";
+		3	: digit = "3";
+		4	: digit = "4";
+		5	: digit = "5";
+		6	: digit = "6";
+		7	: digit = "7";
+		8	: digit = "8";
+		9	: digit = "9";
+		default : digit = "0";
+	endcase
+endtask
+
 // 리셋 여부에 대한 내부 count 동작 선언
 always @ (posedge clk) begin
 	// 리셋 초기화 조건
 	if (rst == 1) begin
 		total_count = 0;
 	end else begin
-		// 999 넘을 경우 초기화 조건
-		if (total_count == 999) begin
+		// 599 넘을 경우 초기화 조건
+		if (total_count == 599) begin
 			total_count = 0;
 			count0 = 0;
 			count1 = 0;
@@ -51,47 +72,11 @@ always @ (posedge clk) begin
 		end
 	end
 
-	case (count0)
-		0	: digit0 = "0";
-		1	: digit0 = "1";
-		2	: digit0 = "2";
-		3	: digit0 = "3";
-		4	: digit0 = "4";
-		5	: digit0 = "5";
-		6	: digit0 = "6";
-		7	: digit0 = "7";
-		8	: digit0 = "8";
-		9	: digit0 = "9";
-		default : digit0 = "0";
-	endcase
-	
-	case (count1)
-		0	: digit1 = "0";
-		1	: digit1 = "1";
-		2	: digit1 = "2";
-		3	: digit1 = "3";
-		4	: digit1 = "4";
-		5	: digit1 = "5";
-		6	: digit1 = "6";
-		7	: digit1 = "7";
-		8	: digit1 = "8";
-		9	: digit1 = "9";
-		default : digit1 = "0";
-	endcase
+	// display를 위해 task 사용
+	digit_converter(count2, digit2);
+	digit_converter(count1, digit1);
+	digit_converter(count0, digit0);
 
-	case (count2)
-		0	: digit2 = "0";
-		1	: digit2 = "1";
-		2	: digit2 = "2";
-		3	: digit2 = "3";
-		4	: digit2 = "4";
-		5	: digit2 = "5";
-		6	: digit2 = "6";
-		7	: digit2 = "7";
-		8	: digit2 = "8";
-		9	: digit2 = "9";
-		default : digit2 = "0";
-	endcase
 end
 
 // count 값에 따른 7-segment 선언
@@ -136,14 +121,11 @@ always @ (negedge clk) begin
 		9 : s2 <= 8'b11100110; //9, d230
 		default: s2 <= 8'b00000000; //error
 	endcase
-
 	// 분과 초 표기, 비바도에서 한글 인식 안돼서 영어로 표기
 	$display("count: [%d] : [%s]min [%s]sec : time: [%d]", total_count, digit2, {digit1,digit0}, $time);
 end
-
 // 최종 output인 seg에 값 할당
 assign seg0 = s0;
 assign seg1 = s1; 
 assign seg2 = s2; 
-
 endmodule
